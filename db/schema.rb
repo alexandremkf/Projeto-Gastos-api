280b5865eef8cf7c9b8fc6b4e5a01f4ec9544641
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-02 00:08:20 -0300

    Alterei para usar campo 'password' simples ao invés de 'password_digest'.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_02_021153) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
