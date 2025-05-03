d200438cb98b7eeb8978ea167aa3b82a5a18cfab
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-02 23:24:07 -0300

    Coloquei a nova rota para o método GET, para poder consultar, no terminal via curl.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # Rota para teste de vida da aplicação
+  # Rota para teste de vida da aplicação.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Rota de usuários aqui:
+  # Rotas de usuários aqui:
+
+  # Rota de usuário create (POST).
   resources :users, only: [:create]
 
-  # Defines the root path route ("/")
+  # Rota de usuário index (GET).
+  resources :users, only: [:index]
+
+  # Rota root (ainda comentada)
   # root "posts#index"
 end
