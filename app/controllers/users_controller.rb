ad1e53c01c2cd184ded873d04637e3ba9c2414d4
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-02 22:28:42 -0300

    Crie o método para poder acessar todos os usuários dentro do controller do user, listando todos eles, mesmo apenas existindo um único.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,24 @@
 class UsersController < ApplicationController
-    # Método de criar o usuário.
-    def create
-        user = User.new(user_params)
-    
-        if user.save
-          render json: user, status: :created
-        else
-          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-    
-      private
-    
-      def user_params
-        params.require(:user).permit(:first_name, :last_name, :password)
-      end
+  # Método para listar todos os usuários, mesmo apenas existindo um único.
+  def index
+    users = User.all
+    render json: users
+  end  
+  
+  # Método de criar o usuário.
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password)
+  end
 end
