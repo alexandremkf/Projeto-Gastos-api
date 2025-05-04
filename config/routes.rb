785a74c9b63d2995deb43b3c3fc44a82e4ea9350
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-03 22:50:27 -0300

    Coloquei a nova rota para o método UPDATE, para poder editar e atualizar os usuários, no terminal via curl.

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,18 @@ Rails.application.routes.draw do
 
   # Rotas de usuários aqui:
 
-  # Rota de usuário create (POST).
-  resources :users, only: [:create]
+    # Rota de usuário create (POST).
+    resources :users, only: [:create]
 
-  # Rota de usuário index (GET).
-  resources :users, only: [:index]
+    # Rota de usuário index (GET).
+    resources :users, only: [:index]
+
+    # Rota de usuário uptade (UPDATE).
+    resources :users, only: [:update]
+    
+  # Rotas de gastos aqui:
+
+
 
   # Rota root (ainda comentada)
   # root "posts#index"
