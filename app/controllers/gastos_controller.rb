db699ccd4435fe6064191ffd899339430fe66294
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-04 14:58:52 -0300

    Coloquei dentro do controller de gastos o before_action, com algumas regras antes de executar algum método.

--- app/controllers/gastos_controller.rb
+++ app/controllers/gastos_controller.rb
@@ -1,2 +1,6 @@
 class GastosController < ApplicationController
+    # Impõe algumas regras antes de executar algum método.
+    before_action :set_user
+    before_action :set_gasto, only: [:show, :update, :destroy]
+
 end
