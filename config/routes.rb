820a0f74f7476390c2ecd23d8ca9f46137a65ddb
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-04 14:45:13 -0300

    Melhorei a forma como estava o código das rotas do usuário, e já coloquei as rotas para os gastos de cada usuário.

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,10 @@ Rails.application.routes.draw do
   # Rota para teste de vida da aplicação.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Rotas de usuários aqui:
-
-    # Rota de usuário create (POST).
-    resources :users, only: [:create]
-
-    # Rota de usuário index (GET).
-    resources :users, only: [:index]
-
-    # Rota de usuário uptade (UPDATE).
-    resources :users, only: [:update]
-
-    # Rota de usuário destroy (DELETE).
-    resources :users, only: [:destroy]
-    
-  # Rotas de gastos aqui:
-
-
+  # Rotas para usuários e seus gastos.
+  resources :users, only: [:create, :index, :update, :destroy] do
+    resources :gastos
+  end
 
   # Rota root (ainda comentada)
   # root "posts#index"
