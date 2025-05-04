f3a216b0a529afdee94559d3e2242237edfa8b2f
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-04 15:04:12 -0300

    Criei o método index, o método para listar todos os gastos, do usuário pedido.

--- app/controllers/gastos_controller.rb
+++ app/controllers/gastos_controller.rb
@@ -3,4 +3,9 @@ class GastosController < ApplicationController
     before_action :set_user
     before_action :set_gasto, only: [:show, :update, :destroy]
 
+    # Método para listar todos os gastos, do usuário pedido.
+    def index
+        render json: @user.gastos
+    end
+
 end
